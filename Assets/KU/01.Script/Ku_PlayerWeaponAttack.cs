63ec3d4fca4b79214db16763f733541c1c6b595b
ironyo <kyou09kang@gmail.com>
2025-08-02 02:16:18 +0900

    Ku_RigidComplete히

--- Assets/KU/01.Script/Ku_PlayerWeaponAttack.cs
+++ Assets/KU/01.Script/Ku_PlayerWeaponAttack.cs
@@ -15,22 +15,19 @@ public class Ku_PlayerWeaponAttack : MonoBehaviour
         playerAttackSound = GetComponent<AudioSource>();
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Enemy"))
         {
             playerAttackSound.Play();
             Transform enemyTransform = collision.transform;
 
-            // 나에서 적으로 가는 방향 벡터
             Vector2 pushDir = (enemyTransform.position - transform.position).normalized;
 
-            // 최종 목표 위치 계산
             Vector3 targetPosition = enemyTransform.position + (Vector3)(pushDir * pushDistance);
 
-            // DOTween으로 자연스럽게 이동
             enemyTransform.DOMove(targetPosition, pushDuration)
-                          .SetEase(Ease.OutQuad); // 밀리듯이 점점 멈추게
+                          .SetEase(Ease.OutQuad);
 
             _enemyScripts = collision.gameObject.GetComponent<H_Enemy>();
             _enemyScripts.Health -= damage;
