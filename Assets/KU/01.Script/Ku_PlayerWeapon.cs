1fba98be9ce07f1bbbf9826d9bc4104d76a91392
chohangyeol09 <198588682+chohangyeol09@users.noreply.github.com>
2025-08-02 04:45:37 +0900

    CHG

--- Assets/KU/01.Script/Ku_PlayerWeapon.cs
+++ Assets/KU/01.Script/Ku_PlayerWeapon.cs
@@ -13,43 +13,17 @@ public class Ku_PlayerWeapon : MonoBehaviour
         playerWeapon.SetActive(false);
     }
 
-
     private void OnEnable()
-    {
-        playerWeapon.SetActive(true);
-
-        float startZ = playerTransform.eulerAngles.z;
-
-        // 공격 시작 각도 설정 (기본 각도 기준)
-        float weaponStartAngle = startZ - AttackStartAngle;
-        float weaponEndAngle = weaponStartAngle + AttackAngle;
-
-        playerWeapon.transform.DOKill();
-        playerWeapon.transform.rotation = Quaternion.Euler(0, 0, weaponStartAngle); // 회전 초기화
-
-        playerWeapon.transform
-            .DORotate(new Vector3(0, 0, weaponEndAngle), rotationDuration, RotateMode.Fast)
-            .SetEase(Ease.OutSine)
-            .OnComplete(() => DeleteWeapon());
-    }
-
-    /*private void OnEnable()
     {
         float startZ = playerTransform.eulerAngles.z;
         playerWeapon.transform.rotation = Quaternion.Euler(0, 0, startZ - AttackStartAngle);
 
-        //playerWeapon.transform.DOKill();
-        //playerWeapon.transform
-        //    .DORotate(new Vector3(0, 0, startZ + 360f), rotationDuration, RotateMode.FastBeyond360)
-        //    .SetEase(Ease.OutSine)
-        //    .OnComplete(() => DeleteWeapon());
-
         playerWeapon.transform.DOKill();
         playerWeapon.transform
-            .DORotate(new Vector3(0, 0, startZ + AttackAngle), rotationDuration, RotateMode.FastBeyond360)
+            .DORotate(new Vector3(0, 0, startZ + 360f), rotationDuration, RotateMode.FastBeyond360)
             .SetEase(Ease.OutSine)
             .OnComplete(() => DeleteWeapon());
-    }*/
+    }
 
     private void DeleteWeapon()
     {
